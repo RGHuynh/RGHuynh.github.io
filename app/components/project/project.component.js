7b15e111725cda1eeb4fc0b6b649a024f34a35d5
RGHuynh <randall.g.huynh@gmail.com>
2018-08-14 13:31:34 -0400

    chore: set main body 'my recent work'

---
+++ app/components/project/project.component.js
@@ -0,0 +1,9 @@
+(function(){
+  var config = {
+    templateUrl: '/app/components/project/project.component.html'
+  };
+  
+  angular
+    .module('rghuynhApp')
+    .component('project', config)
+})();
