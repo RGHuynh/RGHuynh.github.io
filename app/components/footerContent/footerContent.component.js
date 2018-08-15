3185a68bbb7e11a1049719dab7c5d294a22c9bec
RGHuynh <randall.g.huynh@gmail.com>
2018-08-15 10:42:05 -0400

    style: header

---
+++ app/components/footerContent/footerContent.component.js
@@ -0,0 +1,9 @@
+(function(){
+  var config = {
+    templateUrl: '/app/components/footerContent/footerContent.component.html'
+  };
+  
+  angular
+  .module('rghuynhApp')
+  .component('footerContent', config)
+})();
